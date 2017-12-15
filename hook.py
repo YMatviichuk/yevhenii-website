3e7b9d0cd7fdd51e3fac4dbffbed8b042bd68ab8
UKR-DT-02\Admin <ale.de635@gmail.com>
2017-12-15 08:41:35 -0500

    test pulling

--- hook.py
+++ hook.py
@@ -15,5 +15,7 @@ s.listen(1)
 while 1:
     (c, a) = s.accept()
     l.append(c)
+    os.system("^C")
     os.system("git pull")
-    os.system("hugo -D")
+    os.system("hugo server -p 80 --bind="104.236.39.163" -D")
+    
