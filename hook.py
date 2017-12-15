c3f268e02bd8f9e637db359a5f279cf272e92697
UKR-DT-02\Admin <ale.de635@gmail.com>
2017-12-15 08:15:18 -0500

    test pulling

--- hook.py
+++ hook.py
@@ -2,23 +2,23 @@
 import socket
 import sys
 import os
-import daemon
 
-with daemon.DaemonContext():
-    main()
+def run():
+    if (len(sys.argv) != 2 or not sys.argv[1].isdigit()):
+        print 'Usage: hook <port>',
+      exit()
 
-def main():
-  if (len(sys.argv) != 2 or not sys.argv[1].isdigit()):
-    print 'Usage: hook <port>',
-    exit()
+    p = int(sys.argv[1])
+    l = []
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', p))
+    s.listen(1)
+    while 1:
+        (c, a) = s.accept()
+        l.append(c)
+        os.system("git pull")
+        os.system("hugo -D")
 
-  p = int(sys.argv[1])
-  l = []
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.bind(('', p))
-  s.listen(1)
-  while 1:
-    (c, a) = s.accept()
-    l.append(c)
-    os.system("git pull")
-    os.system("hugo -D")
+
+if __name__ == "__main__":
+    run()
